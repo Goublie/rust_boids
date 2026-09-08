ea590106e06211154a1ce338bcae0500ff8b6645
Goublie <lu.le08120@gmail.com>
2026-09-08 19:49:56 +0200

    Implémentation dé l'algorithme des Boids.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-use macroquad::prelude::*;
+use ::macroquad::prelude::*;
+use Zone::*;
 use ::rand::*;
 
 const WIDTH  : f32 = 500.0;
 const HEIGHT : f32 = 500.0;
 
+enum Zone {
+    Repulsion,
+    Orientation,
+    Attraction,
+    Liberte
+}
+
 struct TriangleIsocele {
-    s         :Vec2, //Sommet principal
-    hauteur   :f32, //hauteur du triangle
+    s         : Vec2, //Sommet principal
+    hauteur   : f32, //hauteur du triangle
     vec_dir_h : Vec2,  //vecteur directeur hauteur du triangle
-    largeur   :f32, //logueur de la base
+    largeur   : f32, //logueur de la base
 }
 
 impl TriangleIsocele{
@@ -41,18 +49,23 @@ impl TriangleIsocele{
 }
 
 struct Bird {
-    forme       : TriangleIsocele,
-    vec_vitesse : Vec2,
-    v_max       : f32
+    forme               : TriangleIsocele,
+    vec_vitesse         : Vec2,
+    v_max               : f32,
+    zone_repulsion      : f32,
+    zone_orientation    : f32,
+    zone_attraction     : f32,
 }
 
 impl Bird{
     pub fn new(tete:Vec2) -> Bird{
-        let forme = TriangleIsocele::new(tete, 32.0, 16.0);
         Bird{
-            forme       : forme,
-            vec_vitesse : vec2(0.0,0.0),
-            v_max       : 5.0
+            forme               : TriangleIsocele::new(tete, 32.0, 16.0),
+            vec_vitesse         : vec2(0.0,0.0),
+            v_max               : 5.0,
+            zone_repulsion      : 40.0,
+            zone_orientation    : 60.0,
+            zone_attraction     : 80.0
         }
     }
 
@@ -66,7 +79,6 @@ impl Bird{
         draw_triangle(points.0, points.1, points.2, RED);
     }
 
-
     fn dans_l_ecran(&self) -> bool{
         0.0 < self.forme.s.x && 
         self.forme.s.x < WIDTH && 
@@ -74,13 +86,53 @@ impl Bird{
         self.forme.s.y < HEIGHT
     }
 
-    pub fn avancer(&mut self){
+    fn evaluation_distance(&self, copain : &Bird) -> (Zone,Vec2) {
+        let vec_distance = self.forme.s - copain.forme.s;
+        let distance = vec_distance.length();
+
+        let mut eval = (Liberte,vec_distance);
+
+        if distance < self.zone_repulsion{
+            eval.0 = Repulsion;
+        }
+        else if distance >= self.zone_repulsion && distance < self.zone_orientation {
+            eval.0 = Orientation;
+        }
+        else if distance >= self.zone_orientation && distance < self.zone_attraction {
+            eval.0 = Attraction;
+        }
+        eval
+    }
+
+    //Renvoie le vecteur accélération pour corriger la position de l'oiseau
+    fn observer(&self, oiseaux : &[Bird]) -> Vec2{
+        let mut vec_accel = vec2(0.0, 0.0);
+        for copain in oiseaux {
+                let eval = self.evaluation_distance(copain);
+                
+                match eval.0 {
+                    //S'il est trop proche
+                    Repulsion => vec_accel += 0.1*(eval.1),
+                    //S'il est à bonne distance
+                    Orientation => vec_accel += 0.1*copain.vec_vitesse,
+                    //S'il est éloigné
+                    Attraction => vec_accel -= 0.1*eval.1,
+                    //S'il est trop loin
+                    Liberte => ()
+                }
+        }
+        vec_accel
+    }
+
+    fn step(&mut self, accel_correction : Vec2){
         //On génère une acccélération
-        let accel = vec2(random_range(-1.0..1.0),random_range(-1.0..1.0));
+        let accel = vec2(random_range(-0.5..0.5),random_range(-0.5..0.5));
 
         //On ajoute l'accélération au vecteur vitesse
-        if (self.vec_vitesse + accel).length() < self.v_max {
-            self.vec_vitesse += accel;
+        self.vec_vitesse += accel+accel_correction;
+        
+        if (self.vec_vitesse).length() > self.v_max {
+            self.vec_vitesse = self.vec_vitesse.normalize()*self.v_max;
         }
 
         if !self.dans_l_ecran(){
@@ -92,8 +144,8 @@ impl Bird{
         self.forme.vec_dir_h = self.vec_vitesse.normalize();
             
     }
-}
 
+}
 
 fn fenetre_config() -> Conf {
     Conf {
@@ -116,9 +168,17 @@ async fn main() {
     loop {
         clear_background(BLACK);
 
-        for oiseau in &mut oiseaux {
+    let mut accelerations:Vec<Vec2> = Vec::new();
+
+        //Boucle d'observation
+        for oiseau in &oiseaux{
+            accelerations.push(oiseau.observer(&oiseaux));
+        }
+
+        //Boucle de marche
+        for (oiseau, accel) in &mut oiseaux.iter_mut().zip(accelerations.iter()) {
             oiseau.afficher();
-            oiseau.avancer();
+            oiseau.step(*accel);
         }
 
         next_frame().await
